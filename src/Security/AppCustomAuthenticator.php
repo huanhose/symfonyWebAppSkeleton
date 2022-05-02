fbdb33ea6abdff1ec3cb17035305693103497974
huanho <juanjose.penalva@gmail.com>
2022-05-02 09:00:43 +0200

    On auth success, by default redirect to home

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -49,9 +49,8 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //If user access directly this page been, or not target path, redirect to home
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
