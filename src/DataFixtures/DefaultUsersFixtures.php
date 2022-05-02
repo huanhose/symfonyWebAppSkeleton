ad079317d1441319a9fbf4a5af91b0ac87db4c9a
huanho <juanjose.penalva@gmail.com>
2022-05-02 10:19:07 +0200

    Add fixture to create a default admin user

---
+++ src/DataFixtures/DefaultUsersFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class DefaultUsersFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        //A user administrator
+        $user = new User();
+        $user
+            ->setEmail('admin@webApp.com')
+            ->setName('admin')
+            ->setFullName('Admin user')
+            ->setIsVerified(true);
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            '123456'
+        );
+        $user->setPassword($hashedPassword);       
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+        
+        $manager->flush(); 
+    }
+}
