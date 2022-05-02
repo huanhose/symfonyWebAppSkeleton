6aa334e220c11c84f467d7732d0b566bd530a4a5
huanho <juanjose.penalva@gmail.com>
2022-05-02 10:18:48 +0200

    Add Doctrine fixtures bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
